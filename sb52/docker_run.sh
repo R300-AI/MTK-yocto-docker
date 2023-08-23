3f61a2904c24d98b2f15b462d1802cfef6332d77
Anita Wu <anita.wu@itri.org.tw>
2023-08-23 15:23:21 +0800

    feat: improve docker_run script
    mapping host correct user id and group id to container dev user group id and user id

--- sb52/docker_run.sh
+++ sb52/docker_run.sh
@@ -1,2 +1,12 @@
-#!/bin/sh
-docker run -idt --name $USER-sb52 --rm -v $PWD:/home/dev/sb52-yocto sb52:dev
+#!/bin/bash
+container_name=$USER-sb52
+usrid=`id -u`
+grpid=`id -g`
+
+### check container is ready
+if [ `docker ps | grep -c $container_name`=1 ]; then
+    echo "set dev uid and gid"
+    docker exec -d $container_name usermod -u $uesrid -g $grpid dev
+else
+    docker run -idt --name $container_name --rm -v $PWD:/home/dev/sb52-yocto sb52:dev
+fi 
